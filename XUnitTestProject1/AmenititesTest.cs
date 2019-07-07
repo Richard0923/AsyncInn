6af40e12f72e7c602466ae1f3dbd60f1e00add9b
richard0923 <r.rosadoocasio@gmail.com>
2019-07-07 16:46:52 -0700

    added test for getters and setters

---
+++ XUnitTestProject1/AmenititesTest.cs
@@ -0,0 +1,26 @@
+﻿using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+   public class AmenititesTest
+    {
+        [Fact]
+        public void AmenitiesObjectTest()
+        {
+            Amenities testamenity = new Amenities();
+            var id = 40;
+            var name = "test";
+
+            testamenity.ID = id;
+            testamenity.Name = name;
+
+            Assert.Equal(id, testamenity.ID);
+            Assert.Equal(name, testamenity.Name);
+
+        }
+    }
+}
---
+++ XUnitTestProject1/HotelTest.cs
@@ -0,0 +1,34 @@
+﻿using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class HotelTest
+    {
+        [Fact]
+        public void HotelObjectTest()
+        {
+            //arrange 
+            Hotel testHotel = new Hotel();
+            var id = 20;
+            var name = "TEST HOTEL";
+            var city = "seattle";
+            var state = "WA";
+            //act
+            testHotel.ID = id;
+            testHotel.Name = name;
+            testHotel.City = city;
+            testHotel.State = state;
+            //assert
+            Assert.Equal(id, testHotel.ID);
+            Assert.Equal(name, testHotel.Name);
+            Assert.Equal(city, testHotel.City);
+            Assert.Equal(state, testHotel.State);
+
+        }
+
+    }
+}
