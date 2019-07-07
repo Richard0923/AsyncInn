d90d41c5f0728fc41abb26d311ed08ee99b72562
richard0923 <r.rosadoocasio@gmail.com>
2019-07-07 13:38:13 -0700

    added test(not done) and css(not done)

--- XUnitTestProject1/RoomTest.cs
+++ XUnitTestProject1/RoomTest.cs
@@ -1,11 +1,18 @@
 using AsyncInn.Models;
+using AsyncInn.Models.Interfaces;
 using System;
 using Xunit;
+using Moq;
+using AsyncInn.Models.Services;
+using System.Threading.Tasks;
+using AsyncInn.Controllers;
 
 namespace XUnitTestProject1
 {
     public class RoomTest
     {
+
+        //test the room class
         [Fact]
         public void RoomObjectTest()
         {
@@ -24,6 +31,25 @@ namespace XUnitTestProject1
             Assert.Equal(roomid, roomTest.ID);
             Assert.Equal(roomName, roomTest.Name);
             Assert.Equal(roomlayout, roomTest.Layout);
+        }
+
+        [Fact]
+        public async Task RoomControllerCreateTestAsync()
+        {
+            //need to find a way to test the controller 
+            //arrange 
+            //make test room
+            var id = 22;
+            var name = "testy room";
+            Room roomT = new Room { ID = id, Name = name };
+            var mock = new Mock<IRoomManager>();
+            //setup the call for the mock 
+            mock.Setup(r => r.GetRoomByID(id)).ReturnsAsync(roomT);
+            var roomController = new RoomsController(mock.Object);
+            var placeholder = roomController //test is not complete still need to figure out what im testing for 
+            //act
+
+            //assert
         }
     }
 }
