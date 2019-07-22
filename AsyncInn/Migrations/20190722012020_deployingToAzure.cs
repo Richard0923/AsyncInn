dc679049acc4320377edfa72728c1fb20be8c227
richard0923 <r.rosadoocasio@gmail.com>
2019-07-21 19:00:21 -0700

    final submit

---
+++ AsyncInn/Migrations/20190722012020_deployingToAzure.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class deployingToAzure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amenities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amenities", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Layout = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelRooms",
+                columns: table => new
+                {
+                    HotelID = table.Column<int>(nullable: false),
+                    RoomID = table.Column<int>(nullable: false),
+                    RoomNumber = table.Column<int>(nullable: false),
+                    Rate = table.Column<decimal>(nullable: false),
+                    PetFriendly = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelRooms", x => new { x.RoomID, x.HotelID });
+                    table.ForeignKey(
+                        name: "FK_HotelRooms_Hotels_HotelID",
+                        column: x => x.HotelID,
+                        principalTable: "Hotels",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HotelRooms_Rooms_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Rooms",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomAmenities",
+                columns: table => new
+                {
+                    AmenitiesID = table.Column<int>(nullable: false),
+                    RoomID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomAmenities", x => new { x.RoomID, x.AmenitiesID });
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Amenities_AmenitiesID",
+                        column: x => x.AmenitiesID,
+                        principalTable: "Amenities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Rooms_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Rooms",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Bathroom" },
+                    { 2, "Hot Tub" },
+                    { 3, "Disco Ball" },
+                    { 4, "Windows" },
+                    { 5, "Rats" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "ID", "City", "Name", "Phone", "State" },
+                values: new object[,]
+                {
+                    { 1, "Seattle", "The Sound", "1234567892", "WA" },
+                    { 2, "Miami", "South Beach", "7863589847", "FL" },
+                    { 3, "Arecibo ", "La Isla", "7874567975", "PR" },
+                    { 4, "New York City ", "Empire", "2829877237", "NY" },
+                    { 5, "Los Angeles", "LA", "2678365839", "CA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "ID", "Layout", "Name" },
+                values: new object[,]
+                {
+                    { 1, 0, "Bathroom Not Included" },
+                    { 2, 1, "One Bed No Windows" },
+                    { 3, 2, "Two Bed and 3 Squatters" },
+                    { 4, 0, "Almost a Room" },
+                    { 5, 1, "One bedroom rats included" },
+                    { 6, 2, "A Suite but without the sweetness" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelRooms_HotelID",
+                table: "HotelRooms",
+                column: "HotelID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenities_AmenitiesID",
+                table: "RoomAmenities",
+                column: "AmenitiesID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelRooms");
+
+            migrationBuilder.DropTable(
+                name: "RoomAmenities");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+
+            migrationBuilder.DropTable(
+                name: "Amenities");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# AsyncInn
+# AsyncInn 
+#### Lab13-AsyncInn
+##### *Author: Richard Rosado *
+
+------------------------------
+
+## Description
+This is a C# web application for Hotel Managing using Sql Server and the Entity FrameWork giving you the ability to manage Hotels, Rooms, and the Amenities associated to those rooms. 
+------------------------------
+## Deployment:
+https://asyncinnapp.azurewebsites.net/
+------------------------------
+
+## Getting Started
+Clone this repository to your local machine.
+```
+$ git clone https://github.com/Richard0923/AsyncInn.git
+```
+#### To run the program from Visual Studio:
+Select ```File``` -> ```Open``` -> ```Project/Solution```
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the ```AsyncInn``` directory.
+
+Then select and open ```AsyncInn.sln```
+
+------------------------------
+
+## Visuals
+
+##### Schema
+![screenshot](/AsyncInn/Assets/AsyncInn2.png)
+
+##### Application Start 
+![screenshot](/AsyncInn/Assets/AsyncInnStart.png)
+
+##### Using the Application
+![screenshot](/AsyncInn/Assets/AsyncInnUsing.png)
+
+
+------------------------------
